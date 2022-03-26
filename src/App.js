2b0d4e6eb5136544db1660e87efed8024e88e29f
mohamed-ali-hammami <dali.654@hotmail.fr>
2022-03-26 23:31:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import Hahaha from './Components/Image';
 
 class App extends Component {
   
 
-  
   
   constructor(props) {
     super(props);
@@ -12,12 +11,16 @@ class App extends Component {
       fullname: "Mohamed Ali Hammami",
       bio: "I don't know what i'm doing",
       img:<Hahaha/>,
-      profession: "webdevelopper " }
-    
-  }
-  state={
-    divcontainer:false}
+      profession: "webdevelopper ",
+      count:0}
+    }
+
+
+    state={divcontainer:false}
   render() {
+    const {count}=this.state
+
+
     var Handlechange = e => {
       this.setState({divcontainer:!this.state.divcontainer})
     }
@@ -34,11 +37,22 @@ class App extends Component {
           <p>something i want to say {this.state.bio}</p>
           <p>I want to be {this.state.profession}</p>
           <p>here is a picture . {this.state.img}</p>
+          <h2>{count}</h2>
            </center>
       </div>)}
       </div>
     )
   }
+
+  
+
+  componentDidMount () {
+  this.myInterval = setInterval(()=>{
+    this.setState(prevState => ({
+      count: this.state.count +1
+    }))
+  },1000)
+}
 }
 
 export default App;
