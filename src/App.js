a429db95e0c425f11b2456857e5c40f4876234a4
mohamed-ali-hammami <dali.654@hotmail.fr>
2022-03-26 18:04:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Hahaha from './Components/Image';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+
+  
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullname: "Mohamed Ali Hammami",
+      bio: "I don't know what i'm doing",
+      img:<Hahaha/>,
+      profession: "webdevelopper " }
+    
+  }
+  state={
+    divcontainer:false}
+  render() {
+    var Handlechange = e => {
+      this.setState({divcontainer:!this.state.divcontainer})
+    }
+    const x=this.state.divcontainer;
+
+    return (
+      <div> 
+        <button onClick={Handlechange}>{x?'Hide':'Show'}</button>
+          {
+            x &&(<div>
+        <center>
+          
+          <h1>My name is {this.state.fullname}</h1>
+          <p>something i want to say {this.state.bio}</p>
+          <p>I want to be {this.state.profession}</p>
+          <p>here is a picture . {this.state.img}</p>
+            
+
+        
+          </center>
+      </div>)
+  }
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/Image.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Image from './hahaha.jpg'
+
+function Hahaha() {
+    return (
+        <div> 
+        
+        <img src={Image}/>
+        </div>
+    )}
+
+export default Hahaha
