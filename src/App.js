cec345c19886c3c43204b5bc6656a52492f7fdbc
mohamed-ali-hammami <dali.654@hotmail.fr>
2022-03-28 10:53:44 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,58 +1,58 @@
-import React, { Component, useEffect, useState } from 'react'
-import Hahaha from './Components/Image';
+import React, { Component } from "react";
+import Hahaha from "./Components/Image";
 
 class App extends Component {
-  
 
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      fullname: "Mohamed Ali Hammami",
-      bio: "I don't know what i'm doing",
-      img:<Hahaha/>,
-      profession: "webdevelopper ",
-      count:0}
-    }
+  state = {
+    fullname: "Mohamed Ali Hammami",
+    bio: "I don't know what i'm doing",
+    img: <Hahaha />,
+    profession: "webdevelopper ",
+    count: 0,
+    divcontainer: false,
+  };
+ 
 
-
-    state={divcontainer:false}
-  render() {
-    const {count}=this.state
-
-
-    var Handlechange = e => {
-      this.setState({divcontainer:!this.state.divcontainer})
-    }
-    const x=this.state.divcontainer;
-
-    return (
-      <div> 
-        <button onClick={Handlechange}>{x?'Hide':'Show'}</button>
-          {x &&(<div>
-              
-          <center>
-          
-          <h1 >My name is {this.state.fullname}</h1>
-          <p>something i want to say {this.state.bio}</p>
-          <p>I want to be {this.state.profession}</p>
-          <p>here is a picture . {this.state.img}</p>
-          <h2>{count}</h2>
-           </center>
-      </div>)}
-      </div>
-    )
+  componentDidMount() {
+    this.myInterval = setInterval(() => {
+      this.setState((prevState) => ({
+        count: this.state.count + 1,
+      }));
+    }, 1000);
   }
 
-  
 
-  componentDidMount () {
-  this.myInterval = setInterval(()=>{
-    this.setState(prevState => ({
-      count: this.state.count +1
-    }))
-  },1000)
-}
+
+  Handlechange = () => {
+    return this.setState({
+      divcontainer: !this.state.divcontainer,
+      count: this.state.count = 0,
+    });
+  };
+  
+  render() {
+    const { count } = this.state;
+
+
+    return (
+      <div>
+        <button onClick={this.Handlechange}>
+          {this.state.divcontainer ? "Hide" : "Show"}
+        </button>
+        {this.state.divcontainer && (
+          <div>
+            <center>
+              <h1>My name is {this.state.fullname}</h1>
+              <p>something i want to say {this.state.bio}</p>
+              <p>I want to be {this.state.profession}</p>
+              <p>here is a picture . {this.state.img}</p>
+              <h2>{count}</h2>
+            </center>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
